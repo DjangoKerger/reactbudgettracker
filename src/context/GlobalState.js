a5fc187851a0dd7b74c526093e14c5517359bc71
“First <Kerger06@aol.com>
2020-09-18 09:47:17 -0500

    adding delete function

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -20,8 +20,17 @@ export const GlobalContext = createContext(initialState);
 export const GlobalProvider = ({ children }) => {
     const [state, dispatch] = useReducer(AppReducer, initialState);
 
+    //Actions
+    function deleteTransaction(id) {
+        dispatch({
+            type: 'DELETE_TRANSACTION',
+            payload: id
+        })
+    }
+
     return(<GlobalContext.Provider value={{
-        transactions: state.transaction
+        transactions: state.transaction,
+        deleteTransaction
     }}>
         {children}
     </GlobalContext.Provider>)
